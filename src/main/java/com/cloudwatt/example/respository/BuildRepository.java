5bdfbc6e06e33e00efa8589771dee5273e088ad7
nidhal <idhal.zeghouh@orange.com>
2019-06-13 16:06:29 +0200

    add Entities version1

--- src/main/java/com/cloudwatt/example/respository/BuildRepository.java
+++ src/main/java/com/cloudwatt/example/respository/BuildRepository.java
@@ -1,9 +1,9 @@
-package com.cloudwatt.example.respository;
-
-import com.cloudwatt.example.entity.BuildEntity;
-import com.cloudwatt.example.entity.JobEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BuildRepository extends JpaRepository<BuildEntity, Long> {
-}
+//package com.cloudwatt.example.respository;
+//
+//import com.cloudwatt.example.entity.BuildEntity;
+//import com.cloudwatt.example.entity.JobEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//
+//public interface BuildRepository extends JpaRepository<BuildEntity, Long> {
+//}
--- src/main/java/com/cloudwatt/example/respository/JobRepository.java
+++ src/main/java/com/cloudwatt/example/respository/JobRepository.java
@@ -1,3 +1,4 @@
+/*
 package com.cloudwatt.example.respository;
 
 import com.cloudwatt.example.entity.FolderEntity;
@@ -6,3 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobRepository extends JpaRepository<JobEntity, Long> {
 }
+*/
