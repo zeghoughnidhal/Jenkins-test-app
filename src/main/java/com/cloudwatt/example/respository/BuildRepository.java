3fe9b896dbaa31dd435e4afbd49e804a59fe620f
nidhal <idhal.zeghouh@orange.com>
2019-06-13 16:10:13 +0200

    delete entities from master

--- src/main/java/com/cloudwatt/example/respository/BuildRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.cloudwatt.example.respository;
-//
-//import com.cloudwatt.example.entity.BuildEntity;
-//import com.cloudwatt.example.entity.JobEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//
-//public interface BuildRepository extends JpaRepository<BuildEntity, Long> {
-//}
--- src/main/java/com/cloudwatt/example/respository/SubFolderRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.cloudwatt.example.respository;
-//
-//import com.cloudwatt.example.entity.FolderEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface SubFolderRepository extends JpaRepository<SubFolderRepository, Long> {
-//}
